4bd23cf8be1f118cc389dca84661a5594e55bae8
Apogee <apogee@Apogees-MacBook-Pro.local>
2017-08-18 11:14:50 +0530

    Finally i done it

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        
+        let url = URL (string: "http://usa.asystguard.com:81/excel/")
+        let requestObj = URLRequest(url: url!)
+        webView.loadRequest(requestObj)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    public func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        print("kashee 123")
+        if navigationType == .other  {
+            let url = request.url
+            
+            let fileData = NSData(contentsOf: url!)
+            let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            let documentsDirectory = paths[0]
+            fileData?.write(toFile: "\(documentsDirectory)/\(String(describing: url?.lastPathComponent))", atomically: true)
+        }
+        
+        return true
+    }
+    
+
 
 
 }
